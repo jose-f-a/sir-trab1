4b547bd62309fa6555387cada4f5a7d528795692
José Ferreira <jose-f-a@users.noreply.github.com>
2020-11-18 15:28:51 +0000

    css na pagina de f1

--- pages/f1.php
+++ pages/f1.php
@@ -11,17 +11,49 @@
       href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap"
       rel="stylesheet"
     />
-    <link rel="stylesheet" href="../styles/landing.css" />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css"
+    />
+    <link rel="stylesheet" href="../styles/f1.css" />
+    <link rel="stylesheet" href="../styles/table.css" />
     <title>F1 Portal</title>
   </head>
   <body>
-    <h1>Hey</h1>
-    <button onclick="currentSeason()">Temporada atual</button>
-    <button onclick="currentDriverStandings()">Classificação de Pilotos</button>
-    <button onclick="currentConstructorsStandings()">
-      Classificação de Construtores
-    </button>
+    <nav class="Header">
+      <a id="logo" href="index.html">Sports Portal</a>
+    </nav>
+
+    <div class="grid-container">
+      <div class="side-menu">
+        <button onclick="currentSeason()">Temporada atual</button>
+        <button onclick="currentDriverStandings()">
+          Classificação de Pilotos
+        </button>
+        <button onclick="currentConstructorsStandings()">
+          Classificação de Construtores
+        </button>
+      </div>
+      <div class="content">
+        <div class="table">
+          <div class="row header green">
+            <div class="cell">Name</div>
+            <div class="cell">Age</div>
+            <div class="cell">Occupation</div>
+            <div class="cell">Location</div>
+          </div>
+
+          <div class="row">
+            <div class="cell"></div>
+            <div class="cell"></div>
+            <div class="cell"></div>
+            <div class="cell"></div>
+          </div>
+        </div>
+      </div>
+    </div>
   </body>
+
   <script>
     function currentSeason() {
       var requestOptions = {
